8ae9925a42dc3ddb70b2560d54365f85c218c159
olagbile john <olagbilejohn@gmail.com>
2022-11-14 18:36:25 +0100

    updated

--- takit/models.py
+++ takit/models.py
@@ -1,11 +1,26 @@
-from django.db import models
 from django.conf import settings
+from django.db import models
 
 # Create your models here.
+
+CATEGORY_CHOICES = (
+    ('s', 'Shirt'),
+    ('sw', 'Sport wear'),
+    ('ow', 'Outwear')
+)
+
+LABEL_CHOICES = (
+    ('P', 'primary'),
+    ('S', 'secondary'),
+    ('D', 'danger')
+)
+
 
 class Item(models.Model):
     title = models.CharField(max_length=100)
     price = models.FloatField()
+    category = models.CharField(choices=CATEGORY_CHOICES, max_length=2)
+    label = models.CharField(choices=LABEL_CHOICES, max_length=1)
 
     def __str__(self):
         return self.title
